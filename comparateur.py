1096176083d788304f0ecd5ec9e205d9dba09a3a
soupe-aux-oignons <154595369+soupe-aux-oignons@users.noreply.github.com>
2025-07-23 11:21:40 +0200

    Add files via upload

---
+++ comparateur.py
@@ -0,0 +1,106 @@
+import os
+import face_recognition
+import numpy as np
+import cv2
+import extracteur as ex
+
+def isAFace(path):
+    face_locations = face_recognition.face_locations(face_recognition.load_image_file(path))
+    return face_locations
+
+def comparateur (path1, path2):
+    """prends en argument deux chemins absolus vers un fichier/dossier représentant/contenant des visages 
+    compare les visages présents dans les deux fichiers et renvoie une liste de booleen et de distance entre les visages"""
+    
+    img1_encoding = face_recognition.face_encodings(face_recognition.load_image_file(path1))
+    img2_encoding = face_recognition.face_encodings(face_recognition.load_image_file(path2))
+    print(img1_encoding)
+    test_bool =[]
+    test_dist =[]
+    for encoding in img2_encoding :
+        print(encoding)
+        test_bool.append(face_recognition.compare_faces(img1_encoding,encoding))
+        print(face_recognition.compare_faces(img1_encoding,encoding))
+
+        test_dist.append(face_recognition.face_distance(img1_encoding, encoding))
+        print(face_recognition.face_distance(img1_encoding,encoding))
+
+    
+
+    return test_bool,test_dist
+
+
+def rechercheImageParVisage(visage_path,data_paths):
+    """prend en argument un chemin absolu vers un visage et un dossier servant de base de données d'image
+    renvoie une liste contenant les chemins absolus vers les images de la base de donnée contenant au moins un visage similaire
+    à celui donné en entrée"""
+
+    corresponding_list=[]
+
+    for image_path in data_paths:
+        bool_checks, dist_checks = comparateur(visage_path, image_path)
+        for check in dist_checks:
+            print(check)
+            if check[0]<=0.5:
+                corresponding_list.append(image_path)
+                break
+    return corresponding_list
+    
+
+def classificationVisage(visage_path,data_path,unidentified_path, threshold):
+    """prend en argument un chemin absolu vers un visage, un dossier contenant des sous dossiers agissant comme une
+    base de recenssement d'identité et un dossier servant de recueil aux visages non indentifiés
+    vérifie si le visage appartient à une des identités déja enregitrées, si c'est le cas, sauvegarde une copie du visage
+    dans le sous dossier de l'identité correspondante
+    sinon le visage est copié dans le dossier des visages non identifiés"""
+    visage = cv2.imread(visage_path)
+    directory_path=os.path.abspath(data_path)
+
+    for sous_dir_path in os.listdir(directory_path):
+        sous_dir_path=directory_path+'/'+sous_dir_path
+        if os.path.isdir(sous_dir_path) : 
+            sous_dir_path_load = ex.load(sous_dir_path)
+        else :
+            continue
+        trues=0
+        size_of_directory=0 
+        for image_path in sous_dir_path_load:
+            print(image_path)
+            bool_checks, dist_checks = comparateur(visage_path, image_path)
+            print(dist_checks)
+            for check in dist_checks:
+                if check[0]<=0.5:
+                    trues+=1
+                size_of_directory+=1
+        print(trues)
+        if trues/size_of_directory>=threshold:
+            cv2.imwrite(sous_dir_path+'/'+f"visageconnu{len(sous_dir_path_load)}.jpg",visage)
+            return
+    
+    cv2.imwrite(unidentified_path+'/'+f"visageinconnu{len(unidentified_path)}.jpg",visage)
+
+
+def affichage_liste(list_path):
+    for path in list_path:
+        image=cv2.imread(path)
+        cv2.imshow("image correspondante",image)
+        cv2.waitKey()
+    cv2.destroyAllWindows()
+            
+
+if __name__=="__main__":
+    path1=os.sys.argv[1]
+    #path2=os.sys.argv[2]
+    #unidentifiedpath=os.sys.argv[3]
+    #classificationVisage(ex.load(path1)[0],path2,ex.load(unidentifiedpath),0.4)
+    #liste = rechercheImageParVisage(ex.load(path1)[0],ex.load(path2))
+    #print(liste, len(liste))
+    #affichage_liste(liste)
+
+    #comparateur(ex.load(path1)[0], ex.load(path2)[0])
+
+    print(isAFace(path1))
+
+
+
+
--- extracteur.py
+++ extracteur.py
@@ -3,30 +3,128 @@ from mtcnn.utils.images import load_image, load_images_batch
 from mtcnn.utils.plotting import plot
 import os
 import cv2
+import time
 
-directory = os.fsencode(os.path.abspath(os.sys.argv[1]))
-print(directory)
-img_paths = []
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".JPG"):
-        print(os.path.abspath(os.sys.argv[1]+"/"+filename))
-        img_paths.append(os.path.abspath(os.sys.argv[1]+"/"+filename))
 
-print(img_paths)
 
-detector = MTCNN()
-i=0
-for img_path in img_paths :
-    img_analyse = load_image(img_path)
-    img_save = cv2.imread(img_path)
-    results = detector.detect_faces(img_analyse)
-    for result in results :
-        for item in result.items():
-            if item[0] == "box":
-                x,y,w,h = item[1]
-        face = img_save[y:y+h,x:x+w]
-        cv2.imwrite(f"{os.path.abspath(os.sys.argv[2])}/face.{i}.jpg",face)
-        i+=1
+def isCorrectFile(filepath):
+    """prend en argument un chemin dans l'arborescence
+    renvoie un booléen verifiant si un fichier est un jpg ou png"""
+    path=os.fsdecode(filepath)
+    return (path.endswith(".jpg") or path.endswith(".png") or path.endswith(".JPG") or path.endswith(".PNG")) and (not path.startswith('.'))
+
+def load_file(path):
+    """prend en argument un chemin dans l'arborescence
+    renvoie une liste contenant le chemin absolu du fichier"""
+    return [os.path.abspath(path)]
+
+def load_directory(path):
+    """prend en argument un chemin dans l'arborescence
+    renvoie une liste contenant le chemin absolu de chaque fichier valide dans le dossier étudié"""
+    path_array = []
+    for file in os.listdir(path):
+        filename = os.fsdecode(file)
+        if isCorrectFile(filename):
+            path_array.append(os.path.abspath(os.fsdecode(path)+"/"+filename))
+    return path_array
+
+def load(path):
+    """prend en argument un chemin dans l'arborescence
+    appelle une des deux fonctions de chargement, renvoie une liste de chemin absolu"""
+    if os.path.isfile(path):
+        return load_file(path)
+    elif os.path.isdir(path):
+        return load_directory(path)
+    else:
+        print("Error : path does not lead to a valid file or directory\n")
+        return
+
+
+def sizeAreaRatioCheck (imagecv2,croppedcv2,importance):
+    """prend en argument une image et une zone d'interet de cette image, ainsi qu'un degré d'importance de la zone d'interet
+    renvoie un booléen comparant l'aire d'une image et d'une zone d'interet contenu dans l'image, selon un pourcentage"""
+    h1=imagecv2.shape[0]
+    w1=imagecv2.shape[1]
+
+    imgsize = h1*w1
+
+    h2=croppedcv2.shape[0]
+    w2=croppedcv2.shape[1]
+
+    cropsize = h2*w2
+
+    if cropsize >= imgsize*(importance/100):
+        return True
+    else:
+        return False
     
+def sizeAreaRatio(imagecv2,croppedcv2):
+    h1=imagecv2.shape[0]
+    w1=imagecv2.shape[1]
+
+    imgsize = h1*w1
+
+    h2=croppedcv2.shape[0]
+    w2=croppedcv2.shape[1]
+
+    cropsize = h2*w2
+
+    return (cropsize*100)/imgsize
+
+
+    
+def extract(input_path,detector,importance,margin,output_path):
+    """prend en argument deux chemins absolu de dossier/fichier, un degré d'importance et une taille de marge
+    charge, analyse puis extrait les visages d'images passées en argument sous forme de chemin absolu vers un fichier
+    ou un dossier
+    enregistre ensuite les visages extrait selon leur taille dans un dossier destination si il est spécifié"""
+    img_paths = load(input_path)
+
+    i=0
+    faces=[]
+    paths=[]
+    for img_path in img_paths :
+        img_analyse = load_image(img_path)
+        img_save = cv2.imread(img_path)
+
+        results = detector.detect_faces(img_analyse)
+
+        for result in results :
+            for item in result.items():
+                if item[0] == "box":
+                    x,y,w,h = item[1]
+
+            #newX=x-margin
+            #newY=y-margin
+            centerX = x+(w//2)
+            centerY = y+(h//2)
+            newX = centerX - (h//2)
+            newY =centerY - (h//2)
+            face = img_save[newY:newY+(h), newX:newX+(h)]
+            #face = img_save[newY:newY+h+margin,newX:newX+w+margin]
+            
+            if face.size != 0 and sizeAreaRatioCheck(img_save,face,importance) :
+                
+                    cv2.imwrite(f"{output_path}/face.{i}.jpg",face)
+
+                    faces.append(face)
+                    paths.append(f"{output_path}/face.{i}.jpg")
+
+                    seconds = time.time()
+                    localtime = time.ctime(seconds)
+                    print(i, localtime, img_path)
+                    i+=1
+                    
+
+    return faces,paths
+            
+
+if __name__=="__main__":
+    analysis_directory = os.fsencode(os.path.abspath(os.sys.argv[1]))
+    output_directory = os.path.abspath(os.sys.argv[2])
+    detector = MTCNN()
+
+    seconds = time.time()
+    extract(analysis_directory,detector,2,30,output_directory)
+    print(time.ctime(seconds))
